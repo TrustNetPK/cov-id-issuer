234f275a1849fa73f6e2580c4da01eeef09ba818
saeed_dev <saeed_dev@yahoo.com>
2020-06-21 08:41:44 +0500

    added fake crdentials for auth flow

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, Form, FormGroup, Input, Container } from 'reactstrap';
 import Auth from '../helpers/Auth';
 
 function LoginContainer(props) {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
 
 	const handleSubmit = async () => {
-		await Auth.authenticate();
-		props.history.replace('/vaccination')
+		console.log(email,password)
+		if (email === 'admin@vaccify.pk' && password === 'admin123') 
+		{
+			await Auth.authenticate();
+			props.history.replace('/vaccination')
+		}
+		else {
+			props.history.push('/noauth')
+		}
 	}
 
 	return (
@@ -14,10 +23,10 @@ function LoginContainer(props) {
 			<Form className="w-50 mx-auto mt-5 text-center ">
 				<h1>Please Sign in here</h1>
 				<FormGroup className="mt-2 pt-5">
-					<Input type="email" name="email" id="exampleEmail" placeholder="Please enter your email here" />
+					<Input type="email" name="email" id="exampleEmail" onChange={(e)=>setEmail(e.target.value)} placeholder="Please enter your email here" />
 				</FormGroup>
 				<FormGroup>
-					<Input type="password" name="password" id="examplePassword" placeholder="Please enter your password here" />
+					<Input type="password" name="password" id="examplePassword" onChange={(e)=>setPassword(e.target.value)} placeholder="Please enter your password here" />
 				</FormGroup>
 				<Button className="btn btn-lg btn-primary btn-block mt-4" onClick={handleSubmit}>Submit</Button>
 			</Form>
