d07ab52f042ca4236a7d520a7b644d46f0802d20
iammar7 <maansaridot@gmail.com>
2020-07-14 01:32:33 +0500

    vaccination issued page added

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, Button } from 'reactstrap';
 import { useHistory, useLocation } from 'react-router-dom';
 
+import { globalStyles } from '../assets/styles/globalStyles';
 import Auth from '../helpers/Auth';
 import useWindowDimensions from '../helpers/useWindowDimensions';
 import AgaKhanLogo from '../assets/images/aga-khan-transparent.png';
@@ -30,49 +31,42 @@ const HeaderComponent = () => {
   };
 
   return (
-    <div>
-      <Navbar
-        color="light"
-        light
-        expand="sm"
-        fixed="top"
-        style={{ boxShadow: '0 0px 50px rgba(0, 0, 0, 0.2)' }}>
-        <NavbarBrand className="navbar-brand">
-          {width > 500 ? (
-            <img src={AgaKhanLogo} alt="aga-khan-logo" style={{ width: '220px' }} />
-          ) : (
-              <img src={AgaKhanMiniLogo} alt="aga-khan-logo" style={{ width: '50px' }} />
-            )}
-        </NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav navbar className="ml-auto">
+    <Navbar color="light" light expand="sm" fixed="top" style={globalStyles.navbar}>
+      <NavbarBrand className="navbar-brand">
+        {width > 500 ? (
+          <img src={AgaKhanLogo} alt="aga-khan-logo" style={globalStyles.navbarLogo} />
+        ) : (
+          <img src={AgaKhanMiniLogo} alt="aga-khan-logo" style={globalStyles.navbarLogoMini} />
+        )}
+      </NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav navbar className="ml-auto">
+          <NavItem>
+            <a
+              rel="noopener noreferrer"
+              href="https://github.com/TrustNetPK"
+              target="_blank"
+              className="btn btn-dark text-white px-4 mr-4 my-3 my-sm-0">
+              GitHub
+            </a>
+          </NavItem>
+          {Auth.getAuth() ? (
             <NavItem>
-              <a
-                rel="noopener noreferrer"
-                href="https://github.com/TrustNetPK"
-                target="_blank"
-                className="btn btn-dark text-white px-4 mr-4 my-3 my-sm-0">
-                GitHub
-              </a>
+              <Button color="danger" className="px-4" onClick={handleLogout}>
+                Log Out
+              </Button>
             </NavItem>
-            {Auth.getAuth() ? (
-              <NavItem>
-                <Button color="danger" className="px-4" onClick={handleLogout}>
-                  Log Out
-                </Button>
-              </NavItem>
-            ) : (
-                <NavItem>
-                  <Button color="danger" className="px-4" onClick={handleLogin}>
-                    Log In
-                </Button>
-                </NavItem>
-              )}
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
+          ) : (
+            <NavItem>
+              <Button color="danger" className="px-4" onClick={handleLogin}>
+                Log In
+              </Button>
+            </NavItem>
+          )}
+        </Nav>
+      </Collapse>
+    </Navbar>
   );
 };
 
---
+++ src/components/ProofForm.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Col, FormGroup, Input, Label } from 'reactstrap';
+
+export default function ProofForm(props) {
+  const { name, CNIC, dosage, batch, expiry, vaccName } = props.data;
+
+  return (
+    <Col lg={5}>
+      <h5 className="mb-4 pb-4 mt-2 text-center">Patient Information</h5>
+      <FormGroup row>
+        <Label for="name" sm={2}>
+          Name
+        </Label>
+        <Col sm={10}>
+          <Input type="text" name="name" id="name" value={name} placeholder="Name" disabled />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="cnic" sm={2}>
+          CNIC
+        </Label>
+        <Col sm={10}>
+          <Input type="text" name="cnic" id="cnic" value={CNIC} placeholder="CNIC" disabled />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="batch" sm={2}>
+          Batch
+        </Label>
+        <Col sm={10}>
+          <Input type="text" name="batch" value={batch} placeholder="Batch" disabled />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="dosage" sm={2}>
+          Dosage
+        </Label>
+        <Col sm={10}>
+          <Input
+            type="text"
+            name="dosage"
+            id="dosage"
+            value={dosage}
+            placeholder="Dosage"
+            disabled
+          />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="expiry" sm={2}>
+          Expiry
+        </Label>
+        <Col sm={10}>
+          <Input
+            type="text"
+            name="expiry"
+            id="expiry"
+            value={expiry}
+            placeholder="Expiry"
+            disabled
+          />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="vaccName" sm={2}>
+          Vaccine
+        </Label>
+        <Col sm={10}>
+          <Input
+            type="text"
+            name="vaccName"
+            id="vaccName"
+            value={vaccName}
+            placeholder="Vaccine Name"
+            disabled
+          />
+        </Col>
+      </FormGroup>
+    </Col>
+  );
+}
